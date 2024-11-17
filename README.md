c49676ac365fc7b35e07442c8700d60a73ef3786
Kirill Rekhov <krekhov.dev@gmail.com>
2024-11-18 01:09:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+<img src="https://github.com/iikrllx/ioquake3-install/blob/master/logo/header.webp">
+</div>
+
+## ioquake3
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -e
+
+mkdir ioquake3
+mkdir zip
+mkdir tmp
+
+# download main Linux ioquake3 build
+wget https://files.ioquake3.org/Linux.zip
+
+# download archive with new high-resolution textures
+wget https://files.ioquake3.org/xcsv_hires.zip
+
+unzip Linux.zip -d ioquake3/
+unzip ioquake3/release-linux-x86_64.zip -d ioquake3/
+
+# get all default *.pk3 files from my SFTP server
+sshpass -p '1' sftp sftpuser@5.63.158.181 <<EOF
+lcd ioquake3/baseq3
+cd pk3
+get *.pk3
+bye
+EOF
+
+# copy cd-key for ioquake3 (optional)
+cp misc/q3key ioquake3/baseq3/
+
+# replace the textures from Quake 3 with new high-resolution textures
+unzip xcsv_hires.zip -d tmp/
+mv tmp/*.pk3 ioquake3/baseq3/
+rm -rf tmp
+
+# put all zip archives separately
+find . -path ./zip -prune -o -name "*.zip" -type f -exec mv {} zip/ \;
