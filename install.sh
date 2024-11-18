bcbf83d400323ca83fff58263cfd2cf1053d5c26
Kirill Rekhov <krekhov.dev@gmail.com>
2024-11-18 20:12:38 +0300

    install.sh: add ssh-keyscan command for SFTP server

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ wget https://files.ioquake3.org/xcsv_hires.zip
 unzip Linux.zip -d ioquake3/
 unzip ioquake3/release-linux-x86_64.zip -d ioquake3/
 
+# know this host
+sftpip="5.63.158.181"
+if ! ssh-keygen -F "$sftpip" 2>/dev/null; then
+	ssh-keyscan -H "$sftpip" >> ~/.ssh/known_hosts
+fi
+
 # get all default *.pk3 files from my SFTP server
 sshpass -p '1' sftp sftpuser@5.63.158.181 <<EOF
 lcd ioquake3/baseq3
