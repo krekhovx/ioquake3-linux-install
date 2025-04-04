108f2ace7be9cb28ab0ae334e168014f34843fd0
Kirill Rekhov <krekhov.dev@gmail.com>
2025-04-04 18:59:06 +0300

    install.sh: refactoring

--- install.sh
+++ install.sh
@@ -1,44 +1,61 @@
-#!/bin/bash -e
+#!/bin/sh
 
-default_installation()
+set -e
+
+download_pk3_maps_files()
 {
-mkdir ioquake3
-mkdir zip
-mkdir tmp
+sshpass -p '1' sftp sftpuser@5.63.158.181 <<EOF
+lcd ioquake3/baseq3
+cd pk3/various-maps
+get *.pk3
+bye
+EOF
+}
 
-# download main Linux ioquake3 build
-wget https://files.ioquake3.org/Linux.zip
-
-# download archive with new high-resolution textures
-wget https://files.ioquake3.org/xcsv_hires.zip
-
-unzip Linux.zip -d ioquake3/
-unzip ioquake3/release-linux-x86_64.zip -d ioquake3/
-
-# know this host
-sftpip="5.63.158.181"
-if ! ssh-keygen -F "$sftpip" >/dev/null 2>&1; then
-	ssh-keyscan -H "$sftpip" >> ~/.ssh/known_hosts
-fi
-
-# get all default *.pk3 files from my SFTP server
+download_pk3_files()
+{
 sshpass -p '1' sftp sftpuser@5.63.158.181 <<EOF
 lcd ioquake3/baseq3
 cd pk3
 get *.pk3
 bye
 EOF
+}
 
-# copy cd-key for ioquake3 (optional)
-cp misc/q3key ioquake3/baseq3/
+default_installation()
+{
+	mkdir ioquake3
+	mkdir zip
+	mkdir tmp
 
-# replace the textures from Quake 3 with new high-resolution textures
-unzip xcsv_hires.zip -d tmp/
-mv tmp/*.pk3 ioquake3/baseq3/
-rm -rf tmp
+	# download main Linux ioquake3 build
+	wget https://files.ioquake3.org/Linux.zip
 
-# put all zip archives separately
-find . -path ./zip -prune -o -name "*.zip" -type f -exec mv {} zip/ \;
+	# download archive with new high-resolution textures
+	wget https://files.ioquake3.org/xcsv_hires.zip
+
+	unzip Linux.zip -d ioquake3/
+	unzip ioquake3/release-linux-x86_64.zip -d ioquake3/
+
+	# know this host (SFTP server with main *.pk3 files)
+	sftpip="5.63.158.181"
+	if ! ssh-keygen -F "$sftpip" >/dev/null 2>&1; then
+		ssh-keyscan -H "$sftpip" >> ~/.ssh/known_hosts
+	fi
+
+	# download all default *.pk3 files from my SFTP server
+	download_pk3_files
+
+	# copy cd-key for ioquake3 (optional)
+	cp misc/q3key ioquake3/baseq3/
+
+	# replace the textures from Quake 3 with new high-resolution textures
+	unzip xcsv_hires.zip -d tmp/
+	mv tmp/*.pk3 ioquake3/baseq3/
+	rm -rf tmp
+
+	# put all zip archives separately
+	find . -path ./zip -prune -o -name "*.zip" -type f -exec mv {} zip/ \;
 }
 
 get_various_maps()
@@ -48,19 +65,14 @@ get_various_maps()
 		exit 1
 	fi
 
-# get all various *.pk3 maps from my SFTP server
-sshpass -p '1' sftp sftpuser@5.63.158.181 <<EOF
-lcd ioquake3/baseq3
-cd pk3/various-maps
-get *.pk3
-bye
-EOF
+	# download various *.pk3 maps from my SFTP server
+	download_pk3_maps_files
 }
 
-if [ $# == 0 ]; then
+if [ $# -eq 0 ]; then
 	default_installation
 else
-	if [ "$1" == "--download-maps" ]; then
+	if [ "$1" = "--download-maps" ]; then
 		get_various_maps
 	fi
 fi
